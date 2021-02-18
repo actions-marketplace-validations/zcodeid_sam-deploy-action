eefaf626e93148958e2f33cc07f54b93d9576942
Windu Purnomo <wpwindupurnomo@gmail.com>
2021-02-19 06:59:07 +0700

    check current location

--- entrypoint.sh
+++ entrypoint.sh
@@ -77,5 +77,9 @@ echo "[default]
 output = text
 region = $AWS_REGION" > ~/.aws/config
 
+# just check my location
+pwd
+ls
+
 aws cloudformation package --template-file $TEMPLATE --output-template-file serverless-output.yaml --s3-bucket $AWS_DEPLOY_BUCKET $AWS_BUCKET_PREFIX $FORCE_UPLOAD $USE_JSON
 aws cloudformation deploy --template-file serverless-output.yaml --stack-name $AWS_STACK_NAME $CAPABILITIES $PARAMETER_OVERRIDES $TAGS
